798ad3f0d41b6e073ab476c660e65d4e2740a11a
張 建文 <chang180@gmail.com>
2020-10-10 14:24:29 +0800

    update to day-04

--- resources/views/backend/module.blade.php
+++ resources/views/backend/module.blade.php
@@ -18,13 +18,21 @@
                     <td>刪除</td>
                     <td>操作</td>
                 </tr>
+                @isset($rows)
+                @foreach($rows as $row)
                 <tr>
-                    <td width="">圖片</td>
-                    <td width=""><input type="text" name="" id=""></td>
-                    <td width="10%"><button class="btn btn-success btn-sm">顯示</button></td>
-                    <td width="10%"><button class="btn btn-danger btn-sm">刪除</button></td>
-                    <td width="10%"><button class="btn btn-info btn-sm">編輯</button></td>
+                <td><img src="{{ asset('img/'.$row->img) }}" style="width:300px;height:30px;"></td>
+                <td>{{ $row->text }}</td>
+                    <td width="10%"><button class="btn btn-success btn-sm" data-id={{ $row->id }}>
+                        @if($row->sh==1) 顯示
+                        @else 隱藏
+                        @endif
+                    </button></td>
+                    <td width="10%"><button class="btn btn-danger btn-sm" data-id={{ $row->id }}>刪除</button></td>
+                    <td width="10%"><button class="btn btn-info btn-sm" data-id={{ $row->id }}>編輯</button></td>
                 </tr>
+@endforeach
+                @endisset
             </table>
         </div>
     </div>
