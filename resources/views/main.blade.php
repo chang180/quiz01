4416dedc5ef53a400609cb3c385350ad56045a0f
張 建文 <chang180@gmail.com>
2020-10-30 11:23:16 +0800

    bug fixed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,7 +17,7 @@
     <ul class="list-group">
         @foreach($news as $key=>$new)
         <li class="list-group-item list-group-item-action p-1 news">{{$key+1}} .{{ mb_substr($new->text,0,20,'utf8') }}... 
-        <div class="border border-success text-white rounded-shadow bg-secondary offset-3 d-none position-absolute"> <pre class="text-white">{{$new->text}}</pre> </div>
+        <div class="border border-success text-white rounded-shadow bg-secondary offset-3 d-none position-absolute w-100 text-5"> <pre class="text-white">{{$new->text}}</pre> </div>
         </li>
         @endforeach
     </ul>
